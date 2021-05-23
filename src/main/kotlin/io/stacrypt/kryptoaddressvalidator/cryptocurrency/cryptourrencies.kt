6bf9409834c9c3e4cac3698ea79c1417e2510e4d
M_rey3000 <m_rey3000@yahoo.com>
2021-05-23 22:25:17 +0430

    Add bitcoinCash and litecoin

--- src/main/kotlin/io/stacrypt/kryptoaddressvalidator/cryptocurrency/cryptourrencies.kt
+++ src/main/kotlin/io/stacrypt/kryptoaddressvalidator/cryptocurrency/cryptourrencies.kt
@@ -1,13 +1,39 @@
 package io.stacrypt.kryptoaddressvalidator.cryptocurrency
 
+interface Network {}
 enum class CryptoCurrency {
-    Bitcoin
+    // Bitcoin
+    Bitcoin,
+    BTC,
+
+    // Litecoin
+    Litecoin,
+    LTC,
+
+    // BitcoinCash
+    BitcoinCash,
+    BCH
 }
+//
+//val bech32EncodeValidationCurrencies = mapOf(
+//    "BTC" to "Bitcoin",
+//    "LTC" to "Litecoin",
+//    "dgb" to "DigiByte",
+//    "ada" to "Cardano",
+//    "cro" to "Crypto.com Coin"
+//)
+val bech32EncodeValidationCurrencies = listOf(
+    CryptoCurrency.Bitcoin,
+    CryptoCurrency.Litecoin
+)
 
 @ExperimentalUnsignedTypes
-fun String.isValidCryptoCurrencyAddress(cryptoCurrency: CryptoCurrency
-                                        , network: networks):Boolean {
-    when(cryptoCurrency){
-        CryptoCurrency.Bitcoin -> return isValidBitcoinAddress(network)
+fun String.isValidCryptoCurrencyAddress(
+    cryptoCurrency: CryptoCurrency, network: Network
+): Boolean {
+    return when (cryptoCurrency) {
+        CryptoCurrency.Bitcoin, CryptoCurrency.BTC -> isValidBitcoinAddress(network,cryptoCurrency)
+        CryptoCurrency.Litecoin, CryptoCurrency.LTC -> isValidBitcoinAddress(network,cryptoCurrency)
+        CryptoCurrency.BitcoinCash, CryptoCurrency.BCH -> isValidBitcoinCashAddress(network,cryptoCurrency)
     }
 }
