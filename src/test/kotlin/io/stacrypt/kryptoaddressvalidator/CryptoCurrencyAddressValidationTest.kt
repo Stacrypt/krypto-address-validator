40fd04fe427623cdcc136ff5e2dc5e7f58b52bb4
M_rey3000 <m_rey3000@yahoo.com>
2021-05-30 21:15:50 +0430

    Add ethereum crypto currency tests

--- src/test/kotlin/io/stacrypt/kryptoaddressvalidator/CryptoCurrencyAddressValidationTest.kt
+++ src/test/kotlin/io/stacrypt/kryptoaddressvalidator/CryptoCurrencyAddressValidationTest.kt
@@ -616,4 +616,71 @@ class CryptoCurrencyAddressValidationTest {
         Assert.assertTrue("2MxKEf2su6FGAUfCEAHreGFQvEYrfYNHvL7".isValidCryptoCurrencyAddress(CryptoCurrency.DOGE,
                 DogeCoinNetwork.Testnet))
     }
+
+    @ExperimentalUnsignedTypes
+    @Test
+    fun testEthereumAddressValidator() {
+        Assert.assertTrue("0xe42108cb8dfab7e80e396ceb66469f7c85a53009".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0xdcb5410ef70f59a5a2839fc6d4d0b2ca981f5e2d".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+
+        Assert.assertFalse("0xdcb5410ef70f59a5a2839fc6d4d0b2ca981f5e2h".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+
+        Assert.assertTrue("0xE37c0D48d68da5c5b14E5c1a9f1CFE802776D9FF".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0xa00354276d2fC74ee91e37D085d35748613f4748".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0xAff4d6793F584a473348EbA058deb8caad77a288".isValidCryptoCurrencyAddress(
+            CryptoCurrency.ETH,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0xc6d9d2cd449a754c494264e1809c50e34d64562b".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0x52908400098527886E0F7030069857D2E4169EE7".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0x8617E340B3D01FA5F11F306F4090FD50E238070D".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0xde709f2102306220921060314715629080e2fb77".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0x27b1fdb04752bbc536007a920d24acb045561c26".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359".isValidCryptoCurrencyAddress(
+            CryptoCurrency.ETH,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB".isValidCryptoCurrencyAddress(
+            CryptoCurrency.Ethereum,
+            EthereumNetwork.Mainnet
+        ))
+        Assert.assertTrue("0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb".isValidCryptoCurrencyAddress(
+            CryptoCurrency.ETH,
+            EthereumNetwork.Mainnet
+        ))
+    }
 }
