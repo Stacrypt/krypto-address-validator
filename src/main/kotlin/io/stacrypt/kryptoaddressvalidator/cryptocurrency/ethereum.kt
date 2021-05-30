3277dbf68f4faf2e7765b3825291b9fe9126deee
M_rey3000 <m_rey3000@yahoo.com>
2021-05-30 17:48:48 +0430

    Add ethereum crypto currency

---
+++ src/main/kotlin/io/stacrypt/kryptoaddressvalidator/cryptocurrency/ethereum.kt
@@ -0,0 +1,48 @@
+package io.stacrypt.kryptoaddressvalidator.cryptocurrency
+
+import io.stacrypt.kryptoaddressvalidator.cryptography.keccak256Hash
+import org.komputing.khex.extensions.toNoPrefixHexString
+
+
+enum class EthereumNetwork : Network {
+    Mainnet,
+    Testnet
+}
+
+fun String.isValidEthereumAddress(): Boolean {
+    // Check if it has the basic requirements of an address
+    if (!this.matches("^0x[0-9a-fA-F]{40}$".toRegex())) return false
+
+    // If it's all small caps or all all caps, return true
+    if (this.matches("^0x[0-9a-f]{40}$".toRegex()) || this.matches("^0x[0-9A-F]{40}$".toRegex()))
+        return true
+
+    // Otherwise check each case
+    return verifyChecksum()
+}
+
+private fun String.verifyChecksum(): Boolean {
+    // Check each case
+    val addressWithoutPrefix = this.removePrefix("0x")
+
+    val hashedAddress =
+        addressWithoutPrefix.toLowerCase().toByteArray().keccak256Hash().toNoPrefixHexString()
+
+    for (i in 0 until 40) {
+        // The nth letter should be uppercase if the nth digit of casemap is 1
+        if (
+            (Integer.parseInt(
+                hashedAddress[i].toString(),
+                16
+            ) > 7 && addressWithoutPrefix[i].toString()
+                .toUpperCase() != addressWithoutPrefix[i].toString()) ||
+            (Integer.parseInt(
+                hashedAddress[i].toString(),
+                16
+            ) <= 7 && addressWithoutPrefix[i].toString()
+                .toLowerCase() != addressWithoutPrefix[i].toString())
+        ) return false
+    }
+    return true
+}
+
